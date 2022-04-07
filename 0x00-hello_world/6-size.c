72728d2cc38c616f4d39ef8edace9bd24dc2428b
Samuel <awosamuel@gmail.com>
2022-04-07 05:31:18 -0700

    size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ long long int c;
 char d;
 float e;
 
+printf("size of a char: %d byte(s)\n", sizeof(d));
 printf("size of an int: %d byte(s)\n", sizeof(a));
 printf("size of a long int: %d byte(s)\n", sizeof(b));
 printf("size of a long long int: %d byte(s)\n", sizeof(c));
-printf("size of a char: %d byte(s)\n", sizeof(d));
 printf("size of a float: %d byte(s)\n", sizeof(e));
 return (0);
 }
