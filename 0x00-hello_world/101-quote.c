21dc83f356f75cc07bdbaa0e93f4ceea17c3afa4
Samuel <awosamuel@gmail.com>
2022-04-07 06:28:08 -0700

    quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio>
+#include <unistd.h> 
 /** 
  * main - entry point
  *
@@ -7,5 +8,5 @@
 int main(void)
 {
 write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);	
+return (0);	
 }
