98f5a16fc16d2b452a902b2b9998c5f692936c8a
Samuel <awosamuel@gmail.com>
2022-04-08 08:13:39 -0700

    print_comb4.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success/correct)
+ */
+int main(void)
+{
+int digit1, digit2;
+
+for (digit1 = 0; digit2 < 9; digit1++)
+{
+for (digit1 = digit2 + 1; digit2 < 10; digit2++)
+{
+putchar((digit1 % 10) + '
+putchar
