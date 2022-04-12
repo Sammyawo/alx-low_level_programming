e98e2ad7555a88bb4dc1bfd47cfefe6d0e8e2913
Samuel <awosamuel@gmail.com>
2022-04-12 04:37:32 -0700

    times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,25 +5,21 @@
  * Return: Always 0.
  */
 void times_table(void)
-
 {
 int a;
 int b;
 int c;
-
 for (a = 0; a <= 9; a++)
 {
 for (b = 0; b <= 9; b++)
 {
 c = a * b;
-
 if ((c / 10) == 0)
 {
 if (b == 0)
 {
 _putchar ('0');
 }
-
 if (b != 0)
 {
 _putchar (' ');
@@ -35,12 +31,10 @@ _putchar(',');
 _putchar (' ');
 }
 }
-
 else
 {
 _putchar ((c / 10) + '0');
 _putchar ((c % 10) + '0');
-
 if (b < 9)
 {
 _putchar(',');
@@ -48,7 +42,6 @@ _putchar (' ');
 }
 }
 }
-
 _putchar ('\n');
 }
 
