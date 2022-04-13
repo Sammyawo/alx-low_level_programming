083e8afc99622f85717613c8ed9f8d697380579c
Samuel <awosamuel@gmail.com>
2022-04-13 03:46:04 -0700

    print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -15,11 +15,14 @@ if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
 if (month > 2 && day >= 60)
 {
 day++;
+}
 
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 366 - day);
 }
-else if (month == 2 && day == 60)
+else
+{
+if (month == 2 && day == 60)
 {
 printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 }
