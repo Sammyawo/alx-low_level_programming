fddced40c982eeca3f291f92aaa3b99ec9bfa2b7
Samuel <awosamuel@gmail.com>
2022-04-14 10:57:21 -0700

    print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,17 +7,17 @@
  */
 void print_number(int n)
 {
-unsigned int n;
+unsigned int m;
 if (n < 0)
 {
 _putchar("-");
-n = -n;
+m = -n;
 }
 else
 {
-n = n;
+m = n;
 }
-if (n / 10 != 0)
-print_number(n / 10);
-_putchar((n % 10) + '0');
+if (m / 10 != 0)
+print_number(m / 10);
+_putchar((m % 10) + '0');
 }
