d52031cb7b871eadf58a9b0c7a58bb68cae79d34
Samuel <awosamuel@gmail.com>
2022-04-15 03:18:28 -0700

    print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -14,11 +14,13 @@ if (n < 0)
 _putchar('-');
 n = -n;
 }
-else
+else if (n / 10 != 0)
+{
+print_number(n / 10);
+_putchar((n % 10) + '0');
+}
+else 
 {
 n = n;
 }
-if (n / 10 != 0)
-print_number(n / 10);
-_putchar((n % 10) + '0');
 }
