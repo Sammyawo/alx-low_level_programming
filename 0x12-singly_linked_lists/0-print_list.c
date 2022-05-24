bcd1d9268852a4ddb64854c2aa669a041821f221
Samuel <awosamuel@gmail.com>
2022-05-24 06:23:58 -0700

    print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,7 +1,4 @@
 #include "lists.h"
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 /**
  * print_list - prints all the elements  a list_t list.
  *
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,9 @@
 #ifndef _LISTS_
 #define _LISTS_
 
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 /**
  * struct list_s - singly linked list
  *
