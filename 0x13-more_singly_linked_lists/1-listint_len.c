4b69cf87acbe415afb988173c5925f1baf05b9b2
Samuel <awosamuel@gmail.com>
2022-05-26 03:23:20 -0700

    add_nodeint.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,6 +2,13 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
+
+/**
+ * listint_len - return the  number of elements in a linked list
+ *
+ * @h: linked list of type listint_t to traverse
+ * Return: number of nodes
+ */
 size_t listint_len(const listint_t *h)
 {
 size_t men;
