76655ca7774b933ec886207bdd6fecf58749e77a
Samuel <awosamuel@gmail.com>
2022-06-01 05:54:13 -0700

    main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,9 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <sys/types.h>                                                                                                                                  
-#include <sys/stat.h>                            
-#include <fcntl.h> 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
