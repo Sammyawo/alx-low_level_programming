885bab9216dcae4926ea21e73e8a7c436becd3cc
Samuel <awosamuel@gmail.com>
2022-07-07 08:42:19 -0700

    insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -26,7 +26,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		free(mel);
-		return (add_dnodeint.c(h, n));
+		return (add_dnodeint(h, n));
 	}
 
 	for (i = 0; (i < idx - 1) && headcopy != NULL; i++)
