89e20085b7bcf6dfafc5f87b4c4d287ebb68695d
Samuel <awosamuel1@gmail.com>
2023-11-20 17:17:48 +0100

    updated

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+ 
+int _putchar(char c);  /* An example function declaration */
+char *create_array(unsigned int size, char c);
+#endif
